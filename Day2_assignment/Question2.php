297680429f05119b4ef7a1378db41f42b3a46492
ManoliShah03 <manoli.shah@sigmainfo.net>
2023-02-24 14:53:11 +0530

    my second assignment

---
+++ Day2_assignment/Question2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Question 2</title>
+</head>
+<body>
+    <?php
+        $cars = array('Dastun', 'Honda', 'BMW');
+        sort($cars);
+        echo "<ol type=a>";
+        
+        foreach ($cars as $y)
+        {
+            echo "<li>$y</li>";
+        }
+        echo "</ol>";
+
+        echo "<br> Second Element : ";
+        echo $cars[1];
+
+    ?>
+</body>
+</html>
